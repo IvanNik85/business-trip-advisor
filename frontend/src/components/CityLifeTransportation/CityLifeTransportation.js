81d26b2d7bb161d7a0ed190c0d40aeeb87c54977
andrijaDomazet <domazet.andrija@gmail.com>
2019-06-18 12:53:38 +0200

    Dodata komponenta accomodation

--- frontend/src/components/CityLifeTransportation/CityLifeTransportation.js
+++ frontend/src/components/CityLifeTransportation/CityLifeTransportation.js
@@ -3,19 +3,16 @@ import './CityLifeTransportation.scss'
 import Icon from '../../UI/Icon/Icon'
 import IconAndText from '../../UI/IconAndText/IconAndText'
 
-class CityLifeTransportation extends Component {
-    constructor() {
-        super()
-        this.state = {
+class CityLifeTransportation extends Component {    
+        state = {
             id: 'hidden',
-            transform: true
-        }
-        this.handleClick = this.handleClick.bind(this)
-    }
+           transform: true           
+        }   
 
-    handleClick() {
+    handleClick = () => {
         this.setState({
-           id: this.state.id === 'hidden'? 'visible': 'hidden'           
+           id: this.state.id === 'hidden'? 'visible': 'hidden',               
+           transform: !this.state.transform,
         })
     }
 
@@ -33,17 +30,19 @@ class CityLifeTransportation extends Component {
                 return <IconAndText key={i} iconClass={Object.values(el)} color={'color' + i} title={Object.keys(el)} />
             })
     
-        } else {
+        } else if(title === 'Transportation'){
             icons = iconClassesTransport.map((el, i) => {
     
                 return <IconAndText key={i} iconClass={Object.values(el)} color={'color' + i} title={Object.keys(el)} />
             })
-        }    
+        } else {
+            icons = null;
+        } 
             
         return (
             <div className={contDiv}>
                <div className="title" onClick = {this.handleClick}>
-                    <h1>{title}</h1><span><i class="fas fa-chevron-down"></i></span>
+                    <h1>{title}</h1><span><i style={{transform: this.state.transform ? 'rotate(0deg)': 'rotate(180deg)', transition: '0.4s'}}class="fas fa-chevron-down"></i></span>
                </div>
                 <div className={iconDiv} id = {this.state.id} >
                     {icons}
