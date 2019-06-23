b11ddd226eefb3bfdd5bb68fde08b03d572e1b49
Jolene986 <jojovanovic86@gmail.com>
2019-06-23 23:32:04 +0200

    redux dev tools conected

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,15 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "./store/reducer";
 import thunk from "redux-thunk";
 
-const store = createStore(reducer, applyMiddleware(thunk));
+//for redux dev tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
 const app = (
   <Provider store={store}>
     <BrowserRouter>
